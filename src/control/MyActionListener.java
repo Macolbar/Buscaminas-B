b0312816b00fc6e15dd04a9fa1f54cbf9cd4161c
Macolbar <elgithubdemacolbar@gmail.com>
2018-03-10 23:18:13 +0100

    a esto no le hagais caso por ahora

---
+++ src/control/MyActionListener.java
@@ -0,0 +1,69 @@
+package control;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+
+import modelo.Dificultad;
+import modelo.Tablero;
+
+public class MyActionListener implements ActionListener, MouseListener {
+
+	private ParaBuscaminasUI paraBuscaminasUI;
+	Tablero tablero = new Tablero(Dificultad.MEDIO);
+
+	public MyActionListener(ParaBuscaminasUI paraBuscaminasUI) {
+		super();
+		this.paraBuscaminasUI = paraBuscaminasUI;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		// pone el action listener dependiendo del boton que sea
+		if (((JButton) e.getSource()).getName() == "btnReboot") {
+//			paraBuscaminasUI.rebootGame();
+		} else {
+//			paraBuscaminasUI.pseudoMain(((JButton) e.getSource()).getName());
+			new Desvelador(tablero).desvelarCasilla((JButton) e.getSource());
+			if (tablero.isGanador()) {
+				
+				
+			}
+			if(tablero.isPerdedor()){
+				//contenedor.setBackground(Color.RED);
+					
+				
+			}
+		}
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		
+		
+	}
+
+	public void mouseEntered(MouseEvent e) {
+		
+	}
+
+	public void mouseExited(MouseEvent e) {
+		
+	}
+
+	public void mousePressed(MouseEvent e) {
+		if (e.getButton() == MouseEvent.BUTTON3)
+			new Marcador().marcarCasilla((JButton) e.getSource(), tablero);
+	}
+
+	public void mouseReleased(MouseEvent e) {
+		
+		
+	}
+	
+	
+	
+
+}
